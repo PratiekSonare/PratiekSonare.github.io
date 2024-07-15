af4914d38c4ccc65eda1a543064142ce283c76f0
Pratiek <pratiksonare16@gmail.com>
2024-07-15 21:00:53 +0530

    figuring out console errors

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('../../footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-    });
-});
-
 document.querySelectorAll('.faq').forEach(faq => {
   faq.addEventListener('click', () => {
     faq.classList.toggle('active');
